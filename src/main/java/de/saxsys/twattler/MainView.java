c1eda85273ff8e90f480a3d12751f74e12b72939
Raik Bieniek <raik.bieniek@gmail.com>
2014-01-17 22:34:12 +0100

    Display the main view.

--- src/main/java/de/saxsys/twattler/MainView.java
+++ src/main/java/de/saxsys/twattler/MainView.java
@@ -1,13 +1,24 @@
 package de.saxsys.twattler;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
 import javafx.stage.Stage;
 
 public class MainView extends Application {
 
-  @Override
-  public void start(Stage stage) throws Exception {
+	@Override
+	public void start(Stage stage) throws Exception {
+		SplitPane page = (SplitPane) FXMLLoader.load(MainView.class
+				.getResource("/twaddlerMain.fxml"));
+		Scene scene = new Scene(page);
+		stage.setScene(scene);
+		stage.setTitle("Twaddler");
+		stage.show();
+	}
 
-  }
-
+	public static void main(final String args[]) {
+		launch(args);
+	}
 }
