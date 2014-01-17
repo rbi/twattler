07fff72f0cbee79a2436c5ff9bd309a9117c8ad2
Michael Thiele <michael.thiele@saxsys.de>
2014-01-18 00:37:18 +0100

    GUI and PM init in Controller

--- src/main/java/de/saxsys/twattler/JavaChatDemo.java
+++ src/main/java/de/saxsys/twattler/JavaChatDemo.java
@@ -20,7 +20,6 @@ import javafx.application.Application;
 import org.opendolphin.core.client.ClientDolphin;
 import org.opendolphin.core.client.ClientModelStore;
 import org.opendolphin.core.client.comm.BlindCommandBatcher;
-import org.opendolphin.core.client.comm.CommandBatcher;
 import org.opendolphin.core.client.comm.HttpClientConnector;
 import org.opendolphin.core.client.comm.JavaFXUiThreadHandler;
 import org.opendolphin.core.comm.JsonCodec;
