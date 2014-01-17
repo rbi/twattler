996b72dbbc293a8e2bb68c8d12cf7051826c4c6a
Markus Huber <markus.huber@saxsys.de>
2014-01-17 22:25:56 +0100

    ui erstellt

---
+++ src/main/java/de/saxsys/twattler/MainView.java
@@ -0,0 +1,13 @@
+package de.saxsys.twattler;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+
+  @Override
+  public void start(Stage stage) throws Exception {
+
+  }
+
+}
